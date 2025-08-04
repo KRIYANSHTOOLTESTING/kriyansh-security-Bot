6cfb406c34586117349deb43cc760057170266bb
KRIYANSHTOOLTESTING <chatgptone306@gmail.com>
2025-08-05 04:18:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
-# config.py
+# 📁 config.py
 import time
+
+API_ID = 12345678  # 📝 Replace with your actual API ID
+API_HASH = "your_api_hash_here"  # 📝 Replace with your API HASH
+BOT_TOKEN = "your_bot_token_here"  # 📝 Replace with your bot token
+OWNER_ID = 123456789  # 📝 Replace with your Telegram ID
+
+MONGO_URL = "mongodb+srv://<username>:<password>@cluster.mongodb.net/mydatabase"  # 📝 Replace with your MongoDB URL
+
 START_TIME = time.time()
